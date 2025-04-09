01709d5d6061b1cf260fff3495b6db57f1d6ed86
IftatBhuiyan <iftatbhuiyan100@gmail.com>
2025-04-09 03:31:11 -0400

    Reorganized file structure

--- OneTracker/Views/Health/AddHealthEntryView.swift
+++ OneTracker/Views/Health/AddHealthEntryView.swift
@@ -1,30 +1,6 @@
 import SwiftUI
 
-// Enum to represent the optional metrics that can be tracked
-enum OptionalHealthMetric: String, CaseIterable, Identifiable {
-    case weight = "Weight"
-    case mood = "Mood"
-    case sleep = "Sleep"
-    case restingHR = "Resting HR"
-    case hrv = "HRV"
-    case waterIntake = "Water Intake"
-    case stressLevel = "Stress Level"
-    
-    var id: String { self.rawValue }
-    
-    // Optional: Icon for the menu
-    var iconName: String {
-        switch self {
-        case .weight: return "scalemass"
-        case .mood: return "face.smiling"
-        case .sleep: return "bed.double"
-        case .restingHR: return "heart"
-        case .hrv: return "waveform.path.ecg"
-        case .waterIntake: return "drop"
-        case .stressLevel: return "brain.head.profile"
-        }
-    }
-}
+// Removed OptionalHealthMetric enum (moved to Models/OptionalHealthMetric.swift)
 
 struct AddHealthEntryView: View {
     @Binding var isPresented: Bool
